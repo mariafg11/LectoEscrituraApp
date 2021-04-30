f6ae8917058a58547b756666ff4451d8be03e6d2
Maria Flores Garcia <mariafloresgarcia@gmail.com>
2021-04-30 13:04:07 +0200

    First Screen

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -25,11 +25,23 @@ class _AuthenticateState extends State<Authenticate> {
               child: Column(
                 children: [
                   SizedBox(height: 20.0),
-                  FlatButton(
+                  Image(
+                    image: AssetImage('assets/test.png'),
+                    width: 500.0,
+                  ),
+                  FlatButton.icon(
+                    icon: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                    color: Colors.green[300],
                     onPressed: () {
                       return Home();
                     },
-                    child: Text('Jugar'),
+                    label: Text(
+                      'Jugar',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
                   ),
                 ],
               ),
