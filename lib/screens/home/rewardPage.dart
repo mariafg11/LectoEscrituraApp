db2fd85d11d471da1b36365cc9459786e56c446e
Maria Flores Garcia <mariafloresgarcia@gmail.com>
2022-11-24 12:19:50 +0100

    Dress Game implemented

--- lib/screens/home/rewardPage.dart
+++ lib/screens/home/rewardPage.dart
@@ -11,6 +11,15 @@ class RewardPage extends StatefulWidget {
 
 class _RewardPageState extends State<RewardPage> {
   String avatar = '';
+  String top = '';
+  String pant = '';
+  String hat = '';
+  String shoes = '';
+
+  bool _topOn = false;
+  bool _pantOn = false;
+  bool _hatOn = false;
+  bool _shoesOn = false;
   void initState() {
     super.initState();
     avatar = 'assets/' + widget.user.image + '-cuerpo.png';
@@ -20,14 +29,14 @@ class _RewardPageState extends State<RewardPage> {
     Map _options = new Map();
 
     List<String> images = [
-      'pantalones1.png',
-      'camiseta1.png',
-      'camiseta2.png',
-      'pantalones2.png',
-      'zapatos1.png',
-      'zapatos2.png',
-      'lazo.png',
-      'chaqueta1.png'
+      'pantalon-1.png',
+      'camiseta-1.png',
+      'camiseta-2.png',
+      'pantalon-2.png',
+      'zapatos-1.png',
+      'zapatos-2.png',
+      'gorro-1.png',
+      'camiseta-3.png'
     ];
     return Scaffold(
       appBar: AppBar(
@@ -35,12 +44,46 @@ class _RewardPageState extends State<RewardPage> {
       ),
       body: Column(
         children: [
-          Stack(children: <Widget>[
+          Stack(clipBehavior: Clip.antiAlias, children: <Widget>[
             Image(
+              fit: BoxFit.cover,
+              alignment: Alignment(0, -1),
               image: AssetImage(avatar),
-              width: 300,
+              width: 350,
               height: 500,
             ),
+            _hatOn
+                ? Image(
+                    alignment: Alignment(0.3, -0.1),
+                    image: AssetImage('assets/' + hat),
+                    height: 380,
+                    width: 330,
+                  )
+                : SizedBox(),
+            _topOn
+                ? Image(
+                    image: AssetImage('assets/' + top),
+                    height: 520,
+                    width: 350,
+                  )
+                : SizedBox(),
+            _pantOn
+                ? Image(
+                    image: AssetImage('assets/' + pant),
+                    height: 525,
+                    width: 350,
+                  )
+                : SizedBox(
+                    width: 350,
+                    height: 80,
+                  ),
+            _shoesOn
+                ? Image(
+                    image: AssetImage('assets/' + shoes),
+                    height: 520,
+                    width: 350,
+                  )
+                : SizedBox(),
           ]),
           Expanded(
             child: GridView.builder(
@@ -54,15 +97,43 @@ class _RewardPageState extends State<RewardPage> {
                 itemCount: images.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Card(
+                    color: Colors.amber[50],
                     child: InkWell(
-                      child:
-                          Image(image: AssetImage('assets/' + images[index])),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/' + images[index]),
+                      ),
                       onTap: () {
                         //filtrar que lleva puesto y quitarlo si das otra vez
-                        List<String> aux = avatar.split('.');
-                        String selectDress = aux[0] + '-' + images[index];
+                        List<String> aux = images[index].split('-');
+                        switch (aux[0]) {
+                          case 'camiseta':
+                            setState(() {
+                              _topOn = true;
+                              top = images[index];
+                            });
+                            break;
+                          case 'pantalon':
+                            setState(() {
+                              _pantOn = true;
+                              pant = images[index];
+                            });
+                            break;
+                          case 'zapatos':
+                            setState(() {
+                              _shoesOn = true;
+                              shoes = images[index];
+                            });
+                            break;
+                          case 'gorro':
+                            setState(() {
+                              _hatOn = true;
+                              hat = images[index];
+                            });
 
-                        setState(() => avatar = selectDress);
+                            break;
+                          default:
+                        }
                       },
                     ),
                   );
